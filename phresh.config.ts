e83304dfbe50dd6fcbdeebf84fe043e5b0aceeaa
zohayrslileh <zohayrslileh@gmail.com>
2026-08-23 04:27:36 +0100

    Release Flambo 0.1.9

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   identity: "flambo",
   name: "Flambo",
   description: "The official PhreshOS web browser.",
-  version: "0.1.8",
+    version: "0.1.9",
   icon: "flambo-icon.png",
   buildCommand: "vite-node scripts/build.ts",
   server: {
