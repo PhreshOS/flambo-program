0f1054b7d187bbeed3d83afcbef20db32ac0405c
zohayrslileh <zohayrslileh@gmail.com>
2026-08-30 20:06:39 +0100

    Build Flambo with production React runtime

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,10 @@
 import { externalDependencies } from "@/vite.config"
 import { rm, writeFile } from "node:fs/promises"
 import packageConfig from "@/package.json"
-import { build } from "vite"
+
+process.env.NODE_ENV = "production"
+
+const { build } = await import("vite")
 
 await rm("dist", { recursive: true, force: true })
 
