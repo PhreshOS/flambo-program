2fa340a16dc50f5e3cbf8cd9cba2e71cc4988c13
zohayrslileh <zohayrslileh@gmail.com>
2026-09-11 20:23:03 +0100

    Release 0.1.34

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   identity: "flambo",
   name: "Flambo",
   description: "The official PhreshOS web browser.",
-  version: "0.1.33",
+  version: "0.1.34",
   icon: "icon.png",
   categories: ["Internet"],
   keywords: ["browser", "web", "workspace", "automation"],
